18e49ddf6eab02ff4212af6cf25bd877cf443f75
Abbas Mashayekh <martianboy2005@gmail.com>
2021-08-06 15:42:48 +0430

    test: cover declare queue and exchange in broker

--- test/broker.test.ts
+++ test/broker.test.ts
@@ -1,77 +1,128 @@
+import crypto from 'crypto';
+
 import { expect } from 'chai';
 import amqplib from 'amqplib';
+
 import { brokerOptions } from './config';
 import Broker from '../src/amqplib_compat/broker';
+import { createVhost, deleteVhost } from './rabbitmq-http/client';
 
 describe('Broker', () => {
-  it('can access RabbitMQ', async () => {
-    expect(brokerOptions.connection!.host, 'RABBITMQ_HOST').not.to.be.undefined;
-    const broker = new Broker(brokerOptions);
-    await broker.connect();
-    await broker.shutdown();
-  });
-
-  it('initializes the channel pool', async () => {
-    const broker = new Broker(brokerOptions);
-    await broker.connect();
-    expect(broker.pool).not.to.be.undefined;
-    expect(broker.pool?.isOpen).to.be.true;
-    expect(broker.pool?.numFreeChannels).to.be.equal(brokerOptions.poolSize);
-    await broker.shutdown();
-    expect(broker.pool).to.be.undefined;
-  });
-
-  it('does not connect again using the same connection', async () => {
-    const broker = new Broker(brokerOptions);
-    const p1 = broker.connect();
-    const p2 = broker.connect();
-
-    expect(p1 === p2).to.be.true;
-    await p1;
-    await broker.shutdown();
-  });
-
-  it('throws an error if RabbitMQ unavailable', async () => {
-    const broker = new Broker({
-      connection: {
-        ...brokerOptions.connection!,
-        port: 1234,
-        maxRetries: 0
-      },
-      poolSize: 1
-    });
-    return broker.connect().then(() => {
-      throw new Error('Unexpected connection to invalid AMQP server!');
-    }, ex => {
-      expect(ex.code).to.be.equal('ECONNREFUSED');
-    });
-  });
-
-  it('tries the specified number of times to connect', async () => {
-    const orig = amqplib.connect;
-    let num_called = 0;
-    amqplib.connect = function(url, socketOptions?: any) {
-      num_called += 1;
-      return orig(url, socketOptions);
-    };
-
-    const broker = new Broker({
-      connection: {
-        ...brokerOptions.connection!,
-        port: 1234,
-        maxRetries: 3,
-        retryDelay: 5, // ms
-      },
-      poolSize: 1
-    });
-
-    try {
+  describe('Connection', function() {
+    it('can access RabbitMQ', async () => {
+      expect(brokerOptions.connection!.host, 'RABBITMQ_HOST').not.to.be.undefined;
+      const broker = new Broker(brokerOptions);
       await broker.connect();
-      throw new Error('Unexpected connection to invalid AMQP server!');
-    } catch {
-      expect(num_called).to.be.equal(4);
-    } finally {
-      amqplib.connect = orig;
-    }
+      await broker.shutdown();
+    });
+
+    it('initializes the channel pool', async () => {
+      const broker = new Broker(brokerOptions);
+      await broker.connect();
+      expect(broker.pool).not.to.be.undefined;
+      expect(broker.pool?.isOpen).to.be.true;
+      expect(broker.pool?.numFreeChannels).to.be.equal(brokerOptions.poolSize);
+      await broker.shutdown();
+      expect(broker.pool).to.be.undefined;
+    });
+
+    it('does not connect again using the same connection', async () => {
+      const broker = new Broker(brokerOptions);
+      const p1 = broker.connect();
+      const p2 = broker.connect();
+
+      expect(p1 === p2).to.be.true;
+      await p1;
+      await broker.shutdown();
+    });
+
+    it('throws an error if RabbitMQ unavailable', async () => {
+      const broker = new Broker({
+        connection: {
+          ...brokerOptions.connection!,
+          port: 1234,
+          maxRetries: 0
+        },
+        poolSize: 1
+      });
+      return broker.connect().then(() => {
+        throw new Error('Unexpected connection to invalid AMQP server!');
+      }, ex => {
+        expect(ex.code).to.be.equal('ECONNREFUSED');
+      });
+    });
+
+    it('tries the specified number of times to connect', async () => {
+      const orig = amqplib.connect;
+      let num_called = 0;
+      amqplib.connect = function(url, socketOptions?: any) {
+        num_called += 1;
+        return orig(url, socketOptions);
+      };
+
+      const broker = new Broker({
+        connection: {
+          ...brokerOptions.connection!,
+          port: 1234,
+          maxRetries: 3,
+          retryDelay: 5, // ms
+        },
+        poolSize: 1
+      });
+
+      try {
+        await broker.connect();
+        throw new Error('Unexpected connection to invalid AMQP server!');
+      } catch {
+        expect(num_called).to.be.equal(4);
+      } finally {
+        amqplib.connect = orig;
+      }
+    });
+  });
+
+  describe('Functionality', function() {
+    const vhost = crypto.randomBytes(5).toString('hex');
+
+    const broker = new Broker({
+      ...brokerOptions,
+      connection: {
+        ...brokerOptions.connection!,
+        vhost
+      }
+    });
+
+    before(async function() {
+      await createVhost(vhost);
+
+      await broker.connect();
+      return broker;
+    });
+
+    after(async function() {
+      await broker.shutdown();
+      await deleteVhost(vhost);
+    });
+
+    it('can declare queues', async () => {
+      await broker.queues([{
+        name: 'q1',
+        auto_delete: false,
+        durable: false,
+        exclusive: false
+      }]);
+
+      await broker.pool!.acquireAndRun(ch => ch.checkQueue('q1'));
+    });
+
+    it('can declare exchanges', async () => {
+      await broker.exchanges([{
+        name: 'e1',
+        durable: false,
+        type: 'direct',
+      }]);
+
+      await broker.pool!.acquireAndRun(ch => ch.checkExchange('e1'));
+    });
   });
 });
